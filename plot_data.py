d820b54ae94fb572bb7531a9c0fbbef8b10c4a36
rboman <rboman@penguin>
2023-08-18 15:25:53 -0700

    Plots ROC curves.

--- plot_data.py
+++ plot_data.py
@@ -3,14 +3,35 @@ Plot results from loan prediction.
 """
 
 # mypy: disable-error-code=import
-# pylint: disable=unused-import
 
+import collections
 import pathlib
 import sqlite3
 
 import pandas as pd
-import hvplot.pandas  # noqa
 import holoviews as hv
+from sklearn.metrics import roc_curve
+
+
+def make_roc_curve(table: pd.DataFrame, split: str, predict_label: str,
+                   factor: float):
+    """Compute ROC curve using sklearn."""
+    sub_table = table[table['split'] == split]
+    truth_values = []
+    predict_values = []
+    for row_id in range(len(sub_table.index)):
+        row = sub_table.iloc[row_id]
+        if row['not_fully_paid'] == 1:
+            truth_values.append(1)
+        else:
+            truth_values.append(0)
+        predict_values.append(row[predict_label] * factor)
+    true_pos_rate, false_pos_rate, _ = roc_curve(truth_values, predict_values)
+    result_data = collections.defaultdict(list)
+    for true_p, false_p in zip(true_pos_rate, false_pos_rate):
+        result_data['true_positive'].append(true_p)
+        result_data['false_positive'].append(false_p)
+    return pd.DataFrame(result_data)
 
 
 def runit():
@@ -18,15 +39,38 @@ def runit():
     # Load the prediction data from the database.
     code_folder = pathlib.Path(__file__).resolve().parents[0]
     data_folder = code_folder.parents[0] / 'data'
+    image_folder = code_folder / 'images'
+    if not image_folder.exists():
+        image_folder.mkdir()
     conn = sqlite3.connect(data_folder / 'lending_club_loan.sqlite')
     all_data = pd.read_sql_query('select * from predicted_loans', conn)
-    test_data = all_data[all_data['split'] == 'test']
     hv.extension('bokeh')
-    plot = test_data.hvplot.hist('prediction',
-                                 by='not_fully_paid',
-                                 label='Loan Club',
-                                 alpha=0.7)
-    hv.save(plot, data_folder / 'histogram_plot.png')
+    plot_list = []
+    plot_options = {
+        'width': 600,
+        'height': 600,
+        'show_grid': True,
+        'tools': ['hover']
+    }
+    # Predict using neural network score.
+    predict_scores = make_roc_curve(all_data, 'test', 'prediction', 1.)
+    plot = hv.Curve(predict_scores,
+                    kdims='true_positive',
+                    vdims='false_positive',
+                    label='neural network',
+                    group='Bad Loan Prediction')
+    plot_list.append(plot.opts(**plot_options))
+    # Predict using FICO score only.
+    predict_scores = make_roc_curve(all_data, 'test', 'fico', -1.)
+    plot = hv.Curve(predict_scores,
+                    kdims='true_positive',
+                    vdims='false_positive',
+                    label='FICO only',
+                    group='Bad Loan Prediction')
+    plot_list.append(plot.opts(**plot_options))
+    plot = hv.Overlay(plot_list)
+    plot = plot.opts(legend_position='top_left')
+    hv.save(plot, image_folder / 'roc_curve.html')
 
 
 runit()
